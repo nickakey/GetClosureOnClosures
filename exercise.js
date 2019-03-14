c1fd7adaacbc2b6a153a67abc2da5efa20c8cb52
Nick Akey <nickakey@users.noreply.github.com>
2019-03-13 18:46:44 -0600

    Update exercise.js

--- exercise.js
+++ exercise.js
@@ -1,55 +1,72 @@
-for(var i = 0; i < 3; i++){
-  setTimeout(function(){
-    console.log(i);
-  }, 0)
+function one(){
+  for(var i = 0; i < 3; i++){
+    setTimeout(function(){
+      console.log(i);
+    }, 0)
+  }
 }
+//one();
+//Expected log:
+//Actual log:
+//Why does it log that?¿
+function two(){
+  for(let i = 0; i < 3; i++){
+    setTimeout(function(){
+      console.log(i);
+    }, 0)
+  }
+}
+//two();
 //Expected log:
 //Actual log:
 //Why does it log that?¿
 
-for(let i = 0; i < 3; i++){
-  setTimeout(function(){
-    console.log(i);
-  }, 0)
+function three(){
+  for(var i = 0; i < 3; i++){
+    setTimeout(()=>(
+      console.log(i)
+    ), 0)
+  }
 }
+//three();
 //Expected log:
 //Actual log:
 //Why does it log that?¿
 
-for(var i = 0; i < 3; i++){
-  setTimeout(()=>(
-    console.log(i)
-  ), 0)
+function four(){
+  for(var i = 0; i < 3; i++){
+    setTimeout(function(){
+      var locali = i;
+      return function(){console.log(locali)}
+    }(), 0)
+  }
 }
+//four();
 //Expected log:
 //Actual log:
 //Why does it log that?¿
 
-for(var i = 0; i < 3; i++){
-  setTimeout(function(){
-    var locali = i;
-    return function(){console.log(locali)}
-  }(), 0)
+function five(){
+  for(var i = 0; i < 3; i++){
+    setTimeout(function(locali){
+      return function(){console.log(locali)}
+    }(i), 0)
+  }
 }
-//Expected log:
-//Actual log:
-//Why does it log that?¿
-
-for(var i = 0; i < 3; i++){
-  setTimeout(function(locali){
-    return function(){console.log(locali)}
-  }(i), 0)
-}
+//five();
 //Expected log:
 //Actual log:
 //Why does it log that?
 
-for(var i = 0; i < 3; i++){
-  setTimeout(function(locali){
-    var locali = i;
-    return function(){console.log(locali)}
-  }, 0)
+function six(){
+  for(var i = 0; i < 3; i++){
+    setTimeout(function(locali){
+      var locali = i;
+      return function(){console.log(locali)}
+    }, 0)
+  }
 }
+//six();
 //Expected log:
 //Actual log:
 //Why does it log that?
